81962b7c7752cc964fff27368f2bf4bc5e490a6e
RemedyRide <anand.ea0602@gmail.com>
2025-03-09 14:13:53 +0000

    First Commit

---
+++ src/main/java/distsys/smartoffice1/SmartOffice1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.smartoffice1;
+
+/**
+ *
+ * @author anand
+ */
+public class SmartOffice1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
